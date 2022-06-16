67fcb4098c082b01dbe6ea73fecf178aa5e21cff
최정필 <wjdvlf1407@gmail.com>
2022-06-16 16:51:48 +0900

    guestbook4 주석 추가

--- src/main/java/com/javaex/dao/GuestBookDao.java
+++ src/main/java/com/javaex/dao/GuestBookDao.java
@@ -13,20 +13,22 @@ public class GuestBookDao {
 	
 	@Autowired
 	private SqlSession sqlSession;
-
+	
+	//방문자 1명 추가
 	public int guestAdd(GuestBookVo guestBookVo) {
 		System.out.println("GuestBookDao > guestAdd");
 		int count = sqlSession.insert("guestbook.guestAdd",guestBookVo);
 			
 		return count;
 	}
-
+	
+	//방문자 정보 1명 삭제하기
 	public int guestDelete(int no) {
 		int count = sqlSession.delete("guestbook.guestDelete", no);
 
 		return count;
 	}
-	
+	//방문자 전체목록 가져오기
 	public List<GuestBookVo> guestSelect() {
 		List<GuestBookVo> guestList = sqlSession.selectList("guestbook.selectList");
 		
@@ -34,7 +36,7 @@ public class GuestBookDao {
 
 	}
 	
-	// 사람 가져오기
+	// 방문자 한명만 정보가져오기
 	public GuestBookVo getGuest(int no) {
 		GuestBookVo guestBookVo = sqlSession.selectOne("guestbook.getGuest", no);
 
